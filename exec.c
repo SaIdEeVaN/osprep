43ec8cf75c00bd32b2ce83b6a461c6ce75a06d96
Baviri Setty Sai Deevan <saideevan@gmail.com>
2026-05-15 08:00:15 +0530

    Update exec.c
    I have just made sure that some comments are added

--- exec.c
+++ exec.c
@@ -2,12 +2,9 @@
 #include <unistd.h>
 
 int main() {
-
     printf("Before exec()\n");
-
     execl("/bin/ls", "ls", NULL);
-
+    /*anything after the execl command, nothing executes*/
     printf("After exec()\n");
-
     return 0;
 }
